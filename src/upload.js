1be25dfc0d6fb9d8aa36f5cc6b701aadaa263e83
emmanuelgautier1994 <emmanuel.gre@gmail.com>
2018-03-28 12:22:03 +0200

    First elasticsearch integration

--- src/upload.js
+++ src/upload.js
@@ -1,6 +1,22 @@
 
 import { Set } from 'immutable'
 import FileSaver from 'file-saver'
+import elasticsearch from 'elasticsearch'
+
+var client = new elasticsearch.Client({
+  host: '0.0.0.0:9200',
+  log: 'trace'
+})
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
 
 
 export function indexFile(file,optional_name) {
@@ -8,7 +24,7 @@ export function indexFile(file,optional_name) {
   if (optional_name===undefined) {
     optional_name = file.name
   }
-  
+
   //FileSaver.saveAs(blob, 'icicle_'+optional_name+'.json')
   console.log(JSON.stringify(file))
 }
